3c6834ec4b3e686683cbd923da9ec7e83b52259e
McCambley <jakemccamb16@gmail.com>
2021-07-16 12:01:58 -0400

    Dev: contact and about done

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const navLinks = document.querySelectorAll(".header__link");
+// const navLinks = document.querySelectorAll(".header__link");
 
-const menu = document.querySelector(".header__menu");
+// const menu = document.querySelector(".header__menu");
 
-const checkbox = document.querySelector(".header__menu-btn");
+// const checkbox = document.querySelector(".header__menu-btn");
 
-
-navLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-        checkbox.checked = false;
-    });
-});
+// navLinks.forEach(link => {
+//     link.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//         checkbox.checked = false;
+//     });
+// });
